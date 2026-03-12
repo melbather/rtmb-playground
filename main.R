189007712d35a0c429427fa96ec85a6762578efa
Melissa Bather <melissabather@Melissas-MacBook-Pro.local>
2026-03-12 13:20:03 -0700

    added polynomial regression with RTMB

--- main.R
+++ main.R
@@ -7,15 +7,15 @@ library(RTMB)
 set.seed(42)
 
 n <- 1000
-x <- runif(n, -1, 1)
+x_lin <- runif(n, -1, 1)
 
-beta0 <- 1
-beta1 <- 2
-sigma <- 3
+beta0_lin <- 1
+beta1_lin <- 2
+sigma_lin <- 3
 
-y <- beta0 + beta1 * x + rnorm(n, 0, sigma)
+y_lin <- beta0_lin + beta1_lin * x_lin + rnorm(n, 0, sigma_lin)
 
-sample_data <- list(x = x, y = y)
+sample_data_lin <- list(x = x_lin, y = y_lin)
 
 # Negative log likelihood function
 linreg <- function(params) {
@@ -27,30 +27,67 @@ linreg <- function(params) {
   # and it is "needed to enable automatic simulation and residual calculations from the model object".
   # Not sure I 100% follow this
 
-  getAll(sample_data, params)
-  mu <- beta0 + beta1 * x
-  nll <- -sum(dnorm(y, mu, sigma, log = TRUE))
+  getAll(sample_data_lin, params)
+  mu <- beta0 + beta1 * x_lin
+  nll <- -sum(dnorm(y_lin, mu, sigma, log = TRUE))
   nll
 }
 
 # Param starting vals 
 # (set initial vals for the optimiser)
-parameters <- list(
+parameters_lin <- list(
   beta0 = 0,
   beta1 = 0,
   sigma = 1
 )
 
 # Convert likelihood function into object that the optimiser can use 
-obj <- MakeADFun(linreg, parameters)
+obj_lin <- MakeADFun(linreg, parameters_lin)
 
 # Fit the model
 # Q FOR BEN: would like to know a bit more about what this function is actually doing but not sure how important this is
-opt <- nlminb(obj$par, obj$fn, obj$gr)
+opt_lin <- nlminb(obj_lin$par, obj_lin$fn, obj_lin$gr)
 
 # Get estimates + std errors - pretty accurate!
-sdr <- sdreport(obj)
-sdr
+sdreport(obj_lin)
 
 # 2. POLYNOMIAL REGRESSION ------------------------------------------------------------------
-# TODO
+# Simulate some data to use
+set.seed(42)
+
+x_poly <- runif(n, -1, 1)
+
+beta0_poly <- 1
+beta1_poly <- 2
+beta2_poly <- 3
+sigma_poly <- 3
+
+y_poly <- beta0_poly + beta1_poly * x_poly + beta2_poly * x_poly^2 + rnorm(n, 0, sigma_poly)
+
+sample_data_poly <- list(x = x_poly, y = y_poly)
+
+# Negative log likelihood function
+polyreg <- function(params) {
+  getAll(sample_data_poly, params)
+  mu <- beta0 + beta1 * x_poly + beta2 * x_poly^2
+  nll <- -sum(dnorm(y, mu, sigma, log = TRUE))
+  nll
+}
+
+# Param starting vals 
+# (set initial vals for the optimiser)
+parameters_poly <- list(
+  beta0 = 0,
+  beta1 = 0,
+  beta2 = 0,
+  sigma = 1
+)
+
+# Convert likelihood function into object that the optimiser can use 
+obj_poly <- MakeADFun(polyreg, parameters_poly)
+
+# Fit the model
+opt_poly <- nlminb(obj_poly$par, obj_poly$fn, obj_poly$gr)
+
+# Get estimates + std errors - pretty accurate!
+sdreport(obj_poly)
